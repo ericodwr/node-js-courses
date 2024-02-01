4d8f148cdc074b2f27c3d342eea7b17c7aef6126
ericodwr <ericodr@upnvj.ac.id>
2024-02-01 19:22:13 +0700

    add the proper way for authentication

--- app.js
+++ app.js
@@ -3,7 +3,9 @@ const mongoose = require('mongoose');
 const express = require('express');
 const bodyParser = require('body-parser');
 const session = require('express-session');
+const csrf = require('csurf');
 const MongoDBStore = require('connect-mongodb-session')(session);
+const flash = require('connect-flash');
 
 const errorController = require('./controllers/error');
 const User = require('./models/user');
@@ -15,6 +17,8 @@ const store = new MongoDBStore({
   uri: process.env.API_KEY,
   collection: 'sessions',
 });
+
+const csrfProtection = csrf();
 
 app.set('view engine', 'ejs');
 app.set('views', 'views');
@@ -35,6 +39,9 @@ app.use(
   }),
 );
 
+app.use(csrfProtection);
+app.use(flash());
+
 app.use((req, res, next) => {
   if (!req.session.user) return next();
 
@@ -44,6 +51,12 @@ app.use((req, res, next) => {
       next();
     })
     .catch((err) => console.log(err));
+});
+
+app.use((req, res, next) => {
+  res.locals.isAuthenticated = req.session.isLoggedIn;
+  res.locals.csrfToken = req.csrfToken();
+  next();
 });
 
 app.use('/admin', adminRoutes);
--- controllers/auth.js
+++ controllers/auth.js
@@ -1,22 +1,42 @@
+const bycrypt = require('bcryptjs');
+
 const User = require('../models/user');
 
 exports.getLogin = (req, res, next) => {
   res.render('auth/login', {
     path: '/login',
     pageTitle: 'Login',
-    isAuthenticated: req.session.isLoggedIn,
+    errorMessage: req.flash('error'),
   });
 };
 
 exports.postLogin = (req, res, next) => {
-  User.findById('65b9c88fbc58dd82df8e9241')
+  const { email, password } = req.body;
+
+  User.findOne({ email })
     .then((user) => {
-      req.session.user = user;
-      req.session.isLoggedIn = true;
-      req.session.save((err) => {
-        console.log(err);
-        res.redirect('/');
-      });
+      if (!user) {
+        req.flash('error', 'Invalid email or password!');
+        return res.redirect('/login');
+      }
+
+      bycrypt
+        .compare(password, user.password)
+        .then((doMatch) => {
+          if (doMatch) {
+            req.session.user = user;
+            req.session.isLoggedIn = true;
+            req.session.save((err) => {
+              return res.redirect('/');
+            });
+          } else {
+            req.flash('error', 'Invalid email or password!');
+            res.redirect('/login');
+          }
+        })
+        .catch((err) => {
+          res.redirect('/login');
+        });
     })
     .catch((err) => console.log(err));
 };
@@ -26,4 +46,35 @@ exports.postLogout = (req, res, next) => {
     console.log(err);
     res.redirect('/');
   });
+};
+
+exports.getSignUp = (req, res, next) => {
+  res.render('auth/signup', {
+    path: '/signup',
+    pageTitle: 'Sign Up',
+  });
+};
+exports.postSignUp = (req, res, next) => {
+  const { email, name, password, confirmPassword } = req.body;
+
+  User.findOne({ email })
+    .then((userDoc) => {
+      if (userDoc) return res.redirect('/signup');
+
+      return bycrypt
+        .hash(password, 12)
+        .then((hashedPassword) => {
+          const user = new User({
+            name,
+            email,
+            password: hashedPassword,
+            cart: { items: [] },
+          });
+          return user.save();
+        })
+        .then((result) => {
+          res.redirect('/login');
+        });
+    })
+    .catch((err) => console.log(err));
 };
